bdf73a6249673a78b0fcc3e5654b17e5d951076a
João Vitor M.L <doutnus@tutanota.com>
2024-02-05 17:37:34 -0300

    proyecto finalizado

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<div>
+	<h2>OOOPS!!</h2>
+	<h4>¡NO SE HA ENCONTRADO LA PÁGINA DESEADA! :C</h4>
+	<div class="mt-2">
+		<a class="underline" href="/">Volver</a>
+	</div>
+</div>
+
+<style>
+	div { width: 1410px; margin: 3rem auto; }
+	h2 { font-size: 3rem; text-align: left; }
+	h4 { font-size: 1rem; text-align: left; }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Actividades from '../lib/actividades/Actividades.svelte';
+</script>
+
+<div class="w-full">
+	<img class="w-full h-48 object-cover" src="https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/flying-carpet-viktor-vasnetsov-1880-classical-aesthetics.jpg" alt="">
+</div>
+<main class="mx-auto mt-12">
+	<div>
+		<header class="flex items-center justify-between">
+			<div class="flex flex-col w-full">
+				<h1 class="text-3xl font-bold">JOÃO MARCON</h1>
+				<span class="text-sm text-slate-700">São Paulo, Brasil</span>
+			</div>
+			<div class="flex flex-row-reverse items-center gap-4">
+				<a href="https://github.com/douttnus" target="_blank">
+					<img class="w-6 cursor-pointer" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub icon">
+				</a>
+			</div>
+		</header>
+		<div class="w-full mt-4">
+			<p class="font-normal text-slate-850 text-sm">Desarrollador de Software con 2 años de experiência en el desarrollo de aplicaciones web.</p>
+			<p class="font-normal text-slate-850 text-sm">Empezou en la carrera al final de 2022 y está mejorando cada día para entregar buenas soluciones.</p>
+		</div>
+	</div>
+	<Actividades />
+</main>
+
+<style>
+	main { max-width: 873px; }
+	h1 { font-family: 'Zilla Slab'; }
+	span, p { font-family: 'Fredoka'; }
+</style>
